ba877eadfcb9f94de9e13a0b6ee0501850c1bcf7
Nadeen Poorna <nadeenpoorna405@gmail.com>
2026-05-06 22:55:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ from os import environ
 id_pattern = re.compile(r'^-?\d+$')
 
 
-SESSION = environ.get("SESSION", "UHDFiletoLinksBot")
-API_ID = int(environ.get("API_ID", "0"))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+SESSION = environ.get("SESSION", "NADEENFiletoLinksBot")
+API_ID = int(environ.get("API_ID", "25652418"))
+API_HASH = environ.get("API_HASH", "ea8410db4b3301aa261ba5a9e7e2a62b")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7912521844:AAFO1PJb98V7asqsfg9Fj8TeKxUGomV8awY")
 
 
 PORT = int(environ.get("PORT", "8080"))
@@ -22,15 +22,15 @@ MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 ON_HEROKU = "DYNO" in environ
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://nadeen-link.up.railway.app/")
 
 
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "0"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1003757405063"))
 ADMINS = [
     int(admin) if id_pattern.match(admin) else admin
-    for admin in environ.get("ADMINS", "").split()
+    for admin in environ.get("ADMINS", "5737807853").split()
 ]
 
 
-DATABASE_URI = environ.get("DATABASE_URI", "")
-DATABASE_NAME = environ.get("DATABASE_NAME", "")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb://mongo:WPhYgkSrKGthURgsNdMubOpJSFBPGWxV@metro.proxy.rlwy.net:48917")
+DATABASE_NAME = environ.get("DATABASE_NAME", "PAY")
